bf644278af9b3bfe4cd8f460879fd93a960f09f8
Abraham9511 <lincolnz9511@icloud.com>
2017-06-22 22:36:11 +0800

    update parser

--- app/js/renderer/reload.js
+++ app/js/renderer/reload.js
@@ -6,14 +6,15 @@
 // 当某行因输入过长而显示换行时，行号的显示位置有问题
 exports.reload = (fileIndex) => {
   const value = $('.editor').eq(fileIndex).val();
-  $('.preview').eq(fileIndex).html(value);
+  const hValue = require('./../parser/parser.js')(value);
+  $('.preview').eq(fileIndex).html(hValue);
   const result = $('.editor').eq(fileIndex).val().match(new RegExp('\n', 'g')); // eslint-disable-line
   const countOfReturn = !result ? 0 : result.length;
   let countOfChildren = $('#lineNumber').children().length;
 
   // 根据行号的增减来增加或者减少行号元素
   // 如果当前没有内容，则不显示行号
-  if (value.length === 0) {
+  if (hValue.length === 0) {
     $('#lineNumber').children().remove();
     return true;
   }
